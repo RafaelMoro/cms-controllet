57a54c66629a8e93d85db8822a290305d1654d23
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-07-03 01:53:25 -0600

    fix: update evidencia_fotografica_src attribute to be optional

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -479,8 +479,7 @@ export interface ApiRequisicionEquipoRequisicionEquipo
       ]
     > &
       Schema.Attribute.Required;
-    evidencia_fotografica_src: Schema.Attribute.String &
-      Schema.Attribute.Required;
+    evidencia_fotografica_src: Schema.Attribute.String;
     fecha_ultima_revision: Schema.Attribute.Date;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
